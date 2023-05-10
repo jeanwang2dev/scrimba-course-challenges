bbaa07aa79613f5726a822feb2f0f4e3f5dbdf21
jeanwang2dev <jeanharmsen@yahoo.com>
2023-05-10 13:39:45 -0400

    update messages word

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ export default function App() {
             {
                 messages.length === 0 ?
                 <h1>You're all caught up!</h1> :
-                <h1>You have {messages.length} unread 
-                { messages.length === 1 ? "message" : "messages"} </h1>
+                <h1>You have {messages.length} unread { messages.length === 1 ? "message" : "messages"} </h1>
             }
         </div>
 
