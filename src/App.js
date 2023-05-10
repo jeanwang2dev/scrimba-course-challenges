dd7f3db65832711cf747ce27e99e71a4c73b1e08
jeanwang2dev <jeanharmsen@yahoo.com>
2023-05-10 13:39:04 -0400

    messages conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
-import { useState } from 'react';
-import boxes from "./boxes"
-import Box from "./components/Box"
+import React from "react"
 
-const App = () => {
-    const [squares, setSquares] = useState(boxes)
-    
-    function toggle(id) {
-        //console.log(id)
-        setSquares( prevSquares => {
-            // const index = prevSquares.findIndex( item => item.id === id)
-            // return prevSquares.map( item => {
-            //     let currentItem = {...item}
-            //     if(item.id === id) {
-            //         currentItem.on = !item.on
-            //     }
-            //     return currentItem
-            // })
-            return prevSquares.map( square => {
-                return square.id === id ? {...square, on:!square.on} : square
-            })
-        })
-    }
-
-    const squareElements = squares.map(square => (
-        <Box 
-            key={square.id} 
-            on={square.on} 
-            toggle={() => toggle(square.id)}
-        />
-    ))
-
+export default function App() {
+    const [messages, setMessages] = React.useState(["a"])
+    /**
+     * Challenge:
+     * - If there are no unread messages, display "You're all caught up!"
+     * - If there are > 0 unread messages, display "You have <n> unread
+     *   message(s)"
+     *      - If there's exactly 1 unread message, it should read "message"
+     *        (singular)
+     *   {messages.length === 0 && <p> You're all cauht up!</p>}
+            {messages.length > 0 && <p>You have {messages.length} unread { messages.length === 1 ? "message" : "messages"}</p>}
+     */
     return (
-        <div className="max-w-md mx-auto">
-            <section className="flex flex-wrap">
-                {squareElements}
-            </section>
-            
+        <div>
+            {
+                messages.length === 0 ?
+                <h1>You're all caught up!</h1> :
+                <h1>You have {messages.length} unread 
+                { messages.length === 1 ? "message" : "messages"} </h1>
+            }
         </div>
+
     )
 }
-
-export default App;
