f246868668cebf486950f84259c83477a9fdc191
jeanwang2dev <jeanharmsen@yahoo.com>
2023-06-14 11:34:30 -0400

    HOC example

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React, {useState} from "react"
-import WindowTracker from "./components/WindowTracker"
+import { withExtraPropsAdded } from './components/withExtraPropsAdded'
 
-export default function App() {
-    /**
-     * Challenge:
-     * 1. Create state called `show`, default to `true`
-     * 2. When the button is clicked, toggle `show`
-     * 3. Only display `<WindowTracker>` if `show` is `true`
-     */
-
-    const [show, setShow] = useState(true)
-
-    function toggle(){
-        // console.log('toggle')
-        setShow( prevShow => !prevShow )
-    }
-    
+function App(props) {
+    console.log(props)
     return (
-        <div className="flex flex-col items-center container bg-orange-600 mx-auto py-10">
-            <button onClick={toggle} className="btn">
-                Toggle WindowTracker
-            </button>
-            { show && <WindowTracker />}
-        </div>
+        <div>Hello! {props.name}</div>
     )
 }
+
+export default withExtraPropsAdded(App)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// const sayHelloManyTimes = (times) =>
-//   new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
-
-// const helloDiv = document.createElement("div");
-// helloDiv.innerHTML = sayHelloManyTimes(10).join("<br/>");
-// document.body.append(helloDiv);
-
-// index.js
 import './style.css';
 import React from "react-dom";
 import ReactDOM from 'react-dom/client';
