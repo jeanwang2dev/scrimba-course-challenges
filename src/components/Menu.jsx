afcea8d1beadc31fcd11e1fcc55293c186e346c5
jeanwang2dev <jeanharmsen@yahoo.com>
2023-06-16 10:47:24 -0400

    update Menu and change it to functions component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,33 +1,20 @@
 import React, {Component} from "react"
 import { withToggler } from "./HOCs/withToggler"
 
-class Menu extends Component {
-    state = {
-        show: true
-    }
+function Menu(props) {
     
-    toggleShow = () => {
-        this.setState(prevState => {
-            return {
-                show: !prevState.show
-            }
-        })
-    }
-    
-    render() {
-        return (
-            <div>
-                <button className="border bg-gray-300 p-1.5 rounded" onClick={this.toggleShow}>{this.state.show ? "Hide" : "Show"} Menu </button>
-                <nav className="flex-col mb-3" style={{display: this.state.show ? "flex" : "none"}}>
-                    <h6>Signed in as Coder123</h6>
-                    <a>Your Profile</a>
-                    <a>Your Repositories</a>
-                    <a>Your Stars</a>
-                    <a>Your Gists</a>
-                </nav>
-            </div>
-        ) 
-    }
+    return (
+        <div>
+            <button className="border bg-gray-300 p-1.5 rounded" onClick={props.toggle}>{props.on ? "Hide" : "Show"} Menu </button>
+            <nav className="flex-col mb-3" style={{display: props.on ? "flex" : "none"}}>
+                <h6>Signed in as Coder123</h6>
+                <a>Your Profile</a>
+                <a>Your Repositories</a>
+                <a>Your Stars</a>
+                <a>Your Gists</a>
+            </nav>
+        </div>
+    ) 
 }
 
 export default withToggler(Menu)
