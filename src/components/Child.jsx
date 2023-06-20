6e451cd212a2d29780251f12a0d56567606d9ce6
jeanwang2dev <jeanharmsen@yahoo.com>
2023-06-19 22:00:32 -0400

    make parent and child extend purecomponent

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from "react"
+import React, {PureComponent} from "react"
 
-class GrandChild extends Component {
+class GrandChild extends PureComponent {
     render() {
         console.log("[ ]   [ ]   [ ]   [👶🏻] rendered")
         return (
