620260bff6d21ad94a57a287f3f0acf935681739
jeanwang2dev <jeanharmsen@yahoo.com>
2023-06-30 14:48:02 -0400

    add context to components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from "react"
+import ThemeContext from "../themeContext"
 
 class Button extends Component {
     render() {
+        console.log(this.context)
         return (
-            <button className="light-theme">Switch Theme</button>
+            <button className={this.context + "-theme"}>Switch Theme</button>
         )    
     }
 }
+
+Button.contextType = ThemeContext
 
 export default Button
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React, {Component} from "react"
+import ThemeContext from "../themeContext"
 
 class Header extends Component {
     render() {
+        //const value = this.context
         return (
-            <header className="light-theme">
-                <h2 className="font-bold">Light Theme</h2>
+            <header className={`${this.context}-theme`}>
+                <h2 className="font-bold">{this.context} Theme</h2>
             </header>
         )    
     }
 }
+
+Header.contextType = ThemeContext
 
 export default Header
